fa84efd0d2332f491064d6128847609f96783578
pattapatta <pedael@hotmail.it>
2016-02-29 17:33:09 +0100

    fixed template sintax on vector.cpp

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,16 +1,17 @@
-a
+
 #include "../include/vector.h"
 
 
-using namespace numeric;
 
-void QS::numeric::Vector::calc_wt(){
+template <class T>
+void QS::numeric::Vector<T>::calc_wt(){
    for(int i=0; i<size; i++){
       weight += 1;
    }
 }
 
-void QS::numeric::Vector::calc_lft_1_bit(){
+template <class T>
+void QS::numeric::Vector<T>::calc_lft_1_bit(){
    for(unsigned i=0; i<size; i++){
       if(vec[i]==1)
       leftmost_1_bit = i;
@@ -18,29 +19,41 @@ void QS::numeric::Vector::calc_lft_1_bit(){
    }
 }a
 
-QS::numeric::Vector::Vector(std::size_t t){
+template <class T>
+QS::numeric::Vector<T>::Vector(std::size_t t){
    size = t;
    weight = 0;
    leftmost_1_bit = 0;
-   vec = new std::vector<T>(t);
+   vec = std::vector<T>(t);
 }
 
-QS::numeric::Vector::Vector(std::size_t t, T initial_value){
+template <class T>
+QS::numeric::Vector<T>::Vector(std::size_t t, T initial_value){
    size = t;
    weight = 0;
    leftmost_1_bit = 0;
    vec = new std::vector<T>(t,inizial_value);
 }
 
-const T& QS::numeric::Vector::operator[](unsigned i) const{
+//TODO if size==0 size=t else rise exeption "alredyAllocatedVector"
+template <class T>
+void QS::numeric::Vector<T>::resize(std::size_t t){
+   size = t;
+
+}
+
+template <class T>
+const T& QS::numeric::Vector<T>::operator[](unsigned i) const{
    return vec[i];
 }
 
-T& QS::numeric::Vector::operator[](unsigned i){
+template <class T>
+T& QS::numeric::Vector<T>::operator[](unsigned i){
    return vec[i];
 }
 
-Vector& QS::numeric::Vector::operator+=(const Vector v){
+template <class T>
+Vector& QS::numeric::Vector<T>::operator+=(const Vector v){
     if(size!=v.size)
       return; //TODO rise an exception
 	for(i=0; i<size; i++){
